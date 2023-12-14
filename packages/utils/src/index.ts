8d60f2f91a53c580ec2d4e076c9dd078fddc99bf
supbose <yshxw@qq.com>
2023-12-14 19:21:55 +0800

    perf: update setting.json

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * @Author: supbose
+ * @Create Time: 2023-12-10 23:35:05
+ * @Modified by: supbose@qingshu
+ * @Modified time: 2023-12-14 19:14:21
+ * @Description: qingshu.work
+ */
+
 export function get(x: number, y: number) {
   return x + y;
 }
