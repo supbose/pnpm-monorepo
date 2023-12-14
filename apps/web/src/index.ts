c6eaea2adf58ad4dd28533a2621565e5c025ed31
supbose <yshxw@qq.com>
2023-12-14 20:05:44 +0800

    fix: 修复format-watch 脚本

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -2,7 +2,7 @@
  * @Author: supbose
  * @Create Time: 2023-12-10 23:39:42
  * @Modified by: supbose@qingshu
- * @Modified time: 2023-12-14 19:32:40
+ * @Modified time: 2023-12-14 20:04:42
  * @Description: qingshu.work
  */
 
