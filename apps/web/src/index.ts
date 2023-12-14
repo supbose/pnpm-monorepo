867f13bfbf3e84e8bce8b3316f19b004d7d24485
supbose <yshxw@qq.com>
2023-12-14 19:34:47 +0800

    perf: update settings.json

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,6 +1,13 @@
+/**
+ * @Author: supbose
+ * @Create Time: 2023-12-10 23:39:42
+ * @Modified by: supbose@qingshu
+ * @Modified time: 2023-12-14 19:32:40
+ * @Description: qingshu.work
+ */
+
 import { get } from '@supbose-pnpm/utils';
 function main() {
-  // get(1, 2)
   console.log(get(1, 2));
 }
 main();
